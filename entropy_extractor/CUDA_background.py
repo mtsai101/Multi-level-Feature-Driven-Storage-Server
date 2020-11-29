3890a429be4089eb7a1b2f455a80fa2f8050d731
Minhan Tsai <min@teddy.cs.nthu.edu.tw>
2020-11-29 14:45:16 +0800

    main commit

--- entropy_extractor/CUDA_background.py
+++ entropy_extractor/CUDA_background.py
@@ -128,5 +128,5 @@ class ProcFrameCuda3:
 if __name__=="__main__":
     proc_frame_cuda3 = ProcFrameCuda3(rows,cols,check_res)
     gpu_time_3, n_frames = ProcVid1(proc_frame_cuda3,lr)
-#   print(f'GPU 3 (overlap host and device - attempt 1): {n_frames} frames, {gpu_time_3:.2f} ms/frame')
+
 
