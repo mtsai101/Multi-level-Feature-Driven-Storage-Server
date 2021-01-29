899dc267e93646d01290c08cec76d31012341efb
mtsai101 <potato850920@gmail.com>
2021-01-29 22:28:05 +0800

    clean redundant files

--- __init__.py
+++
@@ -1,5 +0,0 @@
-import ffmpeg
-import numpy as np
-import os
-import time
-from . import analytics
--- cvtest.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import time
-cap = cv2.VideoCapture("/home/min/SmartPole/Pole1/2020-11-10_00-00-00/Pole1_2020-11-10_12-00-01.mp4")
-while True:
-    s = time.time()
-    ret, frame = cap.read()
-    if ret is False:
-        break
-    print(1/(time.time()-s))
